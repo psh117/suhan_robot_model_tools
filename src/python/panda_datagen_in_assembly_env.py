f4aaa9d3a76797bda11e006668dbeff7f9f412b0
Suhan Park <psh117@snu.ac.kr>
2022-05-04 07:46:19 +0000

    update for single model

--- src/python/panda_datagen_in_assembly_env.py
+++ src/python/panda_datagen_in_assembly_env.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, division
-from suhan_robot_model_tools.suhan_robot_model_tools_wrapper_cpp import PlanningSceneCollisionCheck, NameVector, IntVector, TRACIKAdapter, isometry_to_vectors
+from suhan_robot_model_tools import suhan_robot_model_tools_wrapper_cpp
 from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
 import rospy
 import numpy as np
@@ -27,41 +27,53 @@ roscpp_init('suhan', [])
 """
 const std::string & name, const std::string & base_link, const std::string & tip_link, double max_time, double precision, const std::string& URDF_param
 """
-pc = PlanningSceneCollisionCheck()
-names = NameVector()
-dofs = IntVector()
-names.append('panda_arm')
+pc = suhan_robot_model_tools_wrapper_cpp.PlanningSceneCollisionCheck()
+names = suhan_robot_model_tools_wrapper_cpp.NameVector()
+dofs = suhan_robot_model_tools_wrapper_cpp.IntVector()
+names.append('panda_left')
+dofs.append(7)
+names.append('panda_right')
+dofs.append(7)
+names.append('panda_top')
 dofs.append(7)
 pc.set_group_names_and_dofs(names,dofs)
 
-ik_adaptor = TRACIKAdapter('panda_link0', 'panda_hand', 0.1, 1e-6,  '/robot_description')
+tcc = suhan_robot_model_tools_wrapper_cpp.TripleChainConstraintsFunctions()
+tcc.add_trac_ik_adapter('left_arm', 'panda_left_link0','panda_left_hand', 0.1, 1e-6,  '/robot_description')
+tcc.add_trac_ik_adapter('right_arm', 'base','panda_right_hand', 0.1, 1e-6,  '/robot_description')
+tcc.add_trac_ik_adapter('top_arm', 'base','panda_top_hand', 0.1, 1e-6,  '/robot_description')
+
+ik_adaptor = tcc.get_trac_ik_adapter('left_arm')
+# ik_adaptor = suhan_robot_model_tools_wrapper_cpp.TRACIKAdapter('panda_left', 'panda_left_link0', 'panda_left_link8', 0.1, 1e-6, '/robot_description')
 
 q = np.array([0, 0, 0, -pi/2, 0, pi/2, pi/4])
+q_total = np.array([0, 0, 0, -pi/2, 0, pi/2, pi/4]*3)
 
-r = pc.is_valid(q)
+r = pc.is_valid(q_total)
 print ('r',r)
 iso = ik_adaptor.forward_kinematics(q)
 lb = ik_adaptor.get_lower_bound()
 ub = ik_adaptor.get_upper_bound()
 
-d = isometry_to_vectors(iso)
+d = suhan_robot_model_tools_wrapper_cpp.isometry_to_vectors(iso)
 pos = d.first
 quat = d.second
+# print(pos, quat)
+# print(lb, ub)
 
 fk_set = []
 failcount = 0
 
 for i in tqdm(range(args.size)):
     r = False
-    if rospy.is_shutdown() == True:
-        break
     while r == False:
         q_rnd = np.random.uniform(low=lb, high=ub)
-        r = pc.is_valid(q_rnd)
+        q_total[:7] = q_rnd
+        r = pc.is_valid(q_total)
         if r == False:
             failcount += 1
     iso = ik_adaptor.forward_kinematics(q_rnd)
-    d = isometry_to_vectors(iso)
+    d = suhan_robot_model_tools_wrapper_cpp.isometry_to_vectors(iso)
     pos = d.first
     quat = d.second
     
