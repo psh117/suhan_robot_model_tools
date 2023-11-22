1fcdf60d80f952a0d297229d99500c88f2bc68c3
Suhan Park <psh117@snu.ac.kr>
2023-11-22 13:00:42 +0900

    updated srmt python package install

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
+from setuptools import find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['suhan_robot_model_tools'],
-    package_dir={'': 'src/python',
-    'adapter':'src/python'})
+    packages=['srmt', 'srmt.*'])
 
 setup(**setup_args)
